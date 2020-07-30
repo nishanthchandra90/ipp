8c003a03ae5679e0b7efdf99416cba9fd0fc3edb
tshivakumar <tejustpr@gmail.com>
2020-07-30 14:50:46 +0200

    Refactored

--- BusinessAppService/src/main/java/com/tcs/ipp/controller/IPPController.java
+++ BusinessAppService/src/main/java/com/tcs/ipp/controller/IPPController.java
@@ -3,8 +3,8 @@ package com.tcs.ipp.controller;
 import com.tcs.ipp.model.ProjectDTO;
 import com.tcs.ipp.service.ProjectsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +16,10 @@ public class IPPController {
     @Autowired
     private ProjectsRepo projectsRepo;
 
-    @PostMapping("/getMatchedProjects")
-    public List<ProjectDTO> getMatchedProjects(@RequestBody String skills) {
+    @GetMapping("/getMatchedProjects")
+    public List<ProjectDTO> getMatchedProjects(@RequestParam String skills) {
         Map<String, List<String>> projectsSkillsMap = getProjectWithSkills();
-        System.out.println("Sending 2 projects as response...");
+        System.out.println("Sending 2 projects as response..." + skills);
         return projectsRepo.getAllProjects().stream().limit(2).collect(Collectors.toList());
     }
 
@@ -27,7 +27,7 @@ public class IPPController {
         return projectsRepo.getAllProjects().stream().collect(Collectors.toMap(ProjectDTO::getProjectName, ProjectDTO::getRequiredSkills));
     }
 
-    @PostMapping("/getAllProjects")
+    @GetMapping("/getAllProjects")
     public List<ProjectDTO> getAllProjects() {
         System.out.println("Sending all projects as response...");
         return projectsRepo.getAllProjects();
